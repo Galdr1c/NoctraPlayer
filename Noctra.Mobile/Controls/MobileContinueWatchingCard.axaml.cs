b31509abba3ec16fa547b66923dfe36c1400c9bf
Galdr1c <yusuf.41.kocak@gmail.com>
2026-07-02 16:51:30 +0300

    feat: refactor MobileContinueWatchingCard and MobileLiveTvCard to replace Button with Border for touch interactions, enhancing UI responsiveness and maintaining consistent command execution through Tapped events

--- Noctra.Mobile/Controls/MobileContinueWatchingCard.axaml.cs
+++ Noctra.Mobile/Controls/MobileContinueWatchingCard.axaml.cs
@@ -1,12 +1,32 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.VisualTree;
+using Noctra.Models;
+using Noctra.ViewModels;
 
-namespace Noctra.Mobile.Controls
+namespace Noctra.Mobile.Controls;
+
+public partial class MobileContinueWatchingCard : UserControl
 {
-    public partial class MobileContinueWatchingCard : UserControl
+    public MobileContinueWatchingCard()
     {
-        public MobileContinueWatchingCard()
+        InitializeComponent();
+    }
+
+    private void CardContainer_Tapped(object? sender, TappedEventArgs e)
+    {
+        if (e.Handled ||
+            DataContext is not Channel channel ||
+            this.FindAncestorOfType<ItemsControl>()?.DataContext is not MainViewModel viewModel)
         {
-            InitializeComponent();
+            return;
+        }
+
+        if (viewModel.SelectMediaCommand.CanExecute(channel))
+        {
+            viewModel.SelectMediaCommand.Execute(channel);
+            e.Handled = true;
         }
     }
 }
--- Noctra.Mobile/Controls/MobileLiveTvCard.axaml.cs
+++ Noctra.Mobile/Controls/MobileLiveTvCard.axaml.cs
@@ -1,5 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.VisualTree;
+using Noctra.Models;
+using Noctra.ViewModels;
 
 namespace Noctra.Mobile.Controls;
 
@@ -44,5 +48,21 @@ public partial class MobileLiveTvCard : UserControl
     {
         get => GetValue(ShowRemoveFavoriteMenuProperty);
         set => SetValue(ShowRemoveFavoriteMenuProperty, value);
+    }
+
+    private void CardContainer_Tapped(object? sender, TappedEventArgs e)
+    {
+        if (e.Handled ||
+            DataContext is not Channel channel ||
+            this.FindAncestorOfType<ItemsControl>()?.DataContext is not MainViewModel viewModel)
+        {
+            return;
+        }
+
+        if (viewModel.SelectMediaCommand.CanExecute(channel))
+        {
+            viewModel.SelectMediaCommand.Execute(channel);
+            e.Handled = true;
+        }
     }
 }
