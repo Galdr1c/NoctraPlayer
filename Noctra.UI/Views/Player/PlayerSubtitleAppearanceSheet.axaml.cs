2bc966e581f4525c00aedcd4e03a5b3c5386e37b
Galdr1c <yusuf.41.kocak@gmail.com>
2026-09-10 13:46:04 +0300

    refactor(player): add shared subtitle appearance control

---
+++ Noctra.UI/Views/Player/PlayerSubtitleAppearanceSheet.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Noctra.UI.Views.Player;
+
+public partial class PlayerSubtitleAppearanceSheet : UserControl
+{
+    public PlayerSubtitleAppearanceSheet()
+    {
+        InitializeComponent();
+    }
+}
