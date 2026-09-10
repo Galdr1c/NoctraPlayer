359cbbfddbd52caccd08af0774cce822f5f3aab4
Galdr1c <yusuf.41.kocak@gmail.com>
2026-09-10 13:08:36 +0300

    refactor(ui): add shared player quality sheet behavior

---
+++ Noctra.UI/Views/Player/PlayerQualitySheet.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Noctra.UI.Views.Player;
+
+public partial class PlayerQualitySheet : UserControl
+{
+    public PlayerQualitySheet()
+    {
+        InitializeComponent();
+    }
+}
